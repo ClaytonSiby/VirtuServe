4bba678f8aaba5f2b71f7e8c500bc585ada7fd05
Clayton Siby <claytonsiby@gmail.com>
2026-01-21 17:36:38 +0200

    Fix: images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "VirtuServe";
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/VirtuServe" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/VirtuServe" : "",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}` : "",
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
