2a1d9ebd5bdecf8cf559df6365971aa31a29bf6b
Clayton Siby <claytonsiby@gmail.com>
2026-01-21 17:19:45 +0200

    Fix: assets build issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: process.env.NODE_ENV === "production" ? "/VirtuServe" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/VirtuServe/" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/VirtuServe" : "",
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
