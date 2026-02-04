aa29f3c8e7082902555eb7d241f4d413e9ca1aaf
Clayton Siby <claytonsiby@gmail.com>
2026-02-04 19:10:34 +0200

    Update: next config for images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -24,8 +24,6 @@ const nextConfig = {
     },
     productionBrowserSourceMaps: false,
   }),
-  // Enable SWC minification for better performance
-  swcMinify: true,
 };
 
 module.exports = nextConfig;
